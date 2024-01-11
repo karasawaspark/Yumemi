7d2f5e6e42c144b9e7bf57b32df1e458944869a7
spark-03 <>
2024-01-11 14:15:22 +0900

    api動作確認済み

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var WeatherImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func WeatherChange(_ sender: Any) {
+        getWeatherImage()
+    }
+    
+    func getWeatherImage() {
+        let responseWeatherStr = YumemiWeather.fetchWeatherCondition()
+        switch responseWeatherStr {
+        case "sunny" :
+            WeatherImageView.image = UIImage(named:responseWeatherStr)?.withRenderingMode(.alwaysTemplate)
+            self.WeatherImageView.tintColor = .red
+        case "cloudy" :
+            WeatherImageView.image = UIImage(named:responseWeatherStr)?.withRenderingMode(.alwaysTemplate)
+            self.WeatherImageView.tintColor = .gray
+        case "rainy" :
+            WeatherImageView.image = UIImage(named:responseWeatherStr)?.withRenderingMode(.alwaysTemplate)
+            self.WeatherImageView.tintColor = .blue
+        default:
+            break
+        }
+    }
 }
-
+            //
+            //        func drowImageView () {
+            //            WeatherImageView.image = UIImage(named: aaa)
+            //        }
+            //
+            //        func colorImageView () {
+            //            WeatherImageView.image = UIImage(named: aaa)?.withRenderingMode(<#T##renderingMode: UIImage.RenderingMode##UIImage.RenderingMode#>)
+            
+            
+ 
