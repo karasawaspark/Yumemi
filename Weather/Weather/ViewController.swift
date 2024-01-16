fccce1491132280ca2bd679433f1a631c76fee31
spark-03 <>
2024-01-16 16:36:22 +0900

    json完了

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    
+    
     let yumemitenki = YumemiTenki()
     
     @IBOutlet weak var WeatherImageView: UIImageView!
@@ -30,39 +34,39 @@ class ViewController: UIViewController {
 }
 
 extension ViewController:YumemiDelegate {
+    func setWeatherTempMax(max: Int) {
+           self.maxTempLabel.text = String(max)
+    }
+
+    func setWeatherTempMin(min: Int) {
+        self.minTempLabel.text = String(min)
+    }
+    
     func setErrorMessage(alert: String) {
         let alertController = UIAlertController(title: alert, message: "エラー", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "閉じる", style: .default, handler: nil))
         present(alertController, animated: false, completion: nil)
     }
     
-/*
- 1行目,alertControllerにダイアログボックスの情報を記載（preferredStyleとは・・・？）
- 2行目,次のアクションを記載（今回のケースは"閉じる"）
- 3行目,表示アニメーション形式を記載
- ※titleはダイアログに入る文言が入るが、引数をそのまま入力できる。
- ※handlerはフォントのようなレイアウトではなく、関数で置き換えられるような機能的な情報を追加する際に使う。特にない場合はnilを使う
- */
-    
-        func setWeatherImage(type: String) {
-            var imageName = "sunny"
-            var tintColor = UIColor.red
-            
-            switch type {
-            case "sunny":
-                imageName = "sunny"
-                tintColor = UIColor.red
-            case "cloudy":
-                imageName = "cloudy"
-                tintColor = UIColor.gray
-            case "rainy":
-                imageName = "rainy"
-                tintColor = UIColor.blue
-            default:
-                break
-            }
-            
-            WeatherImageView.image = UIImage(named: imageName)
-            WeatherImageView.tintColor = tintColor
+    func setWeatherImage(type: String) {
+        var imageName = "sunny"
+        var tintColor = UIColor.red
+        
+        switch type {
+        case "sunny":
+            imageName = "sunny"
+            tintColor = UIColor.red
+        case "cloudy":
+            imageName = "cloudy"
+            tintColor = UIColor.gray
+        case "rainy":
+            imageName = "rainy"
+            tintColor = UIColor.blue
+        default:
+            break
         }
+        
+        WeatherImageView.image = UIImage(named: imageName)
+        WeatherImageView.tintColor = tintColor
     }
+}
--- Weather/Weather/weatherControl2.swift
+++ Weather/Weather/weatherControl2.swift
@@ -7,23 +7,39 @@
 
 import UIKit
 import YumemiWeather
+import Foundation
 //YumemiWeatherのAPIをインポートした
 
 protocol YumemiDelegate {
-    func setWeatherImage(type:String)
-    func setErrorMessage(alert:String)
+    func setWeatherImage(type: String)
+    func setErrorMessage(alert: String)
+    func setWeatherTempMax(max: Int)
+    func setWeatherTempMin(min: Int)
 }
-/*Delegateを実行するためprotocolを記述（Delegateは別のViewControllerを渡すことができるクラスみたいなもの）
-*/
 
 class YumemiTenki {
     var delegate: YumemiDelegate?
-    
+    let requestJson = """
+   {
+    "area":"tokyo",
+   "date":"2020-04-01T12:00:00+09:00"
+   }
+   """
     func setYumemiWeather() {
         do{
-            let responseWeatherStr = try YumemiWeather.fetchWeatherCondition(at: "")
-            //ある一定の確率でエラーを出すAPIを読み込んでいる
-            self.delegate?.setWeatherImage(type: responseWeatherStr)
+            let responseWeatherCondition = try YumemiWeather.fetchWeather(requestJson)
+            
+            guard let yumemiJsonConvert = responseWeatherCondition.data(using: .utf8),
+                  let items = try JSONSerialization.jsonObject(with: yumemiJsonConvert, options: []) as? [String: Any],
+                  let maxTemp = items["max_temperature"] as? Int,
+                  let minTemp = items["min_temperature"] as? Int,
+                  let weatherImage = items["weather_condition"] as? String
+            else{
+                return
+            }
+            self.delegate?.setWeatherTempMax(max: maxTemp)
+            self.delegate?.setWeatherTempMin(min: minTemp)
+            self.delegate?.setWeatherImage(type: weatherImage)
         }catch YumemiWeatherError.unknownError{
             self.delegate?.setErrorMessage(alert: "不明なエラーが発生しました")
         }catch let error{
@@ -31,3 +47,6 @@ class YumemiTenki {
         }
     }
 }
+
+//guared letはエラーにならなかった場合そのまま突っ走るよという。もしエラーが出た場合はreturnで処理を終わらせて、行き止まりにさせてしまうもの。(if letと似ている)
+
